6a0b7a3709cefb778d016dde1f0dfc04672d8b34
Ankit Sati <ankitsati794@gmail.com>
2018-03-18 22:38:05 +0530

    Support Vector Machines

---
+++ SVM_Kernels_With_CVXOPT.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+from sklearn.cluster import KMeans
+
+x = np.array([[1,2],
+            [1.5,1.8],
+            [5,8],
+            [8,8],
+            [1,0.6],
+            [9,11]])
+
+# plt.scatter(x[:, 0], x[:, 1], s=150)
+# plt.show()
+
+clf = KMeans(n_clusters=6)
+clf.fit(x)
+
+centroids = clf.cluster_centers_
+labels = clf.labels_
+ 
+colors = 10*['g.', 'r.', 'c.', 'b.', 'k.']
+
+for i in range(len(x)):
+    plt.plot(x[i][0], x[i][1], colors[labels[i]], markersize = 25)
+plt.scatter(centroids[:,0], centroids[:,1], marker='x', s=150, linewidths=5)
+plt.show()
+
